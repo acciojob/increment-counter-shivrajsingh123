2b392acdab63ea5b3f904aea8193ce49c2bef416
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-03-12 16:07:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function () {
-    let counter = 0;
+    let counter =0;
 
     const counterElement = document.getElementById("counter");
     const incrementBtn = document.getElementById("incrementBtn");
